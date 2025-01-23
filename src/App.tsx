d478dac8a105ea85ce8591be0e85d6c923026af3
Moshniaga Artem <artem.m89@mail.ru>
2025-01-23 12:21:43 +0300

    feat: add description

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
 export const App = () => {
-  return <h1 className="text-3xl font-bold text">Hello World!</h1>;
+  return (
+    <div className="w-[400px]">
+      <h1 className="text-3xl font-bold text">Hello World!</h1>
+      <p className="text-lg text-blue-700">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus
+        numquam repudiandae dignissimos quos quia! Molestiae aliquid tempore
+        sint, incidunt esse eligendi molestias nihil iure perspiciatis, nulla
+        harum sapiente exercitationem doloremque.
+      </p>
+    </div>
+  );
 };
