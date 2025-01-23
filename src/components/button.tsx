7eff2024aef35e252e4fc3480bcb7cc8ac9cf572
Moshniaga Artem <artem.m89@mail.ru>
2025-01-23 14:06:56 +0300

    fix: tag element, add button props

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,13 +1,15 @@
 import { FC } from "react";
 
-interface ButtonProps {
+interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
 }
 
-export const Button: FC<ButtonProps> = ({ children }) => {
+export const Button: FC<ButtonProps> = ({ children, ...props }) => {
   return (
-    <div className="h-11 py-2 px-4 bg-amber-600 border rounded-xl inline-block cursor-pointer hover:bg-amber-500 transition-colors text-white">
+    <button
+      {...props}
+      className="h-11 py-2 px-4 bg-amber-600 border rounded-xl inline-block cursor-pointer hover:bg-amber-500 transition-colors text-white">
       {children}
-    </div>
+    </button>
   );
 };
